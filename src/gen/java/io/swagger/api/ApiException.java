bdab99bd4f9d6e376c2179a810343c51c57097df
Stefano Nassi GFT <stefano.nassi@gft.com>
2024-03-26 18:36:07 +0100

    clear

--- src/gen/java/io/swagger/api/ApiException.java
+++
@@ -1,9 +0,0 @@
-package io.swagger.api;
-
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJerseyServerCodegen", date = "2024-03-26T17:30:36.473922Z[Etc/UTC]")public class ApiException extends Exception{
-    private int code;
-    public ApiException (int code, String msg) {
-        super(msg);
-        this.code = code;
-    }
-}
--- src/gen/java/io/swagger/api/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package io.swagger.api;
-
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJerseyServerCodegen", date = "2024-03-26T17:30:36.473922Z[Etc/UTC]")public class NotFoundException extends ApiException {
-    private int code;
-    public NotFoundException (int code, String msg) {
-        super(code, msg);
-        this.code = code;
-    }
-}
